196f06aafccd7dd193d417f5bf01214db51aa777
ahmedkrdzalic <ahmed.krdzalic@zendev.se>
2023-06-27 15:30:26 +0200

    stringify

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -48,7 +48,7 @@ export const Login = () => {
         if (result?.user) {
           setUser(result?.user);
           localStorage.setItem("user", JSON.stringify(result?.user));
-          localStorage.setItem("token", JSON.stringify(result?.user?.token));
+          localStorage.setItem("token", result?.user?.token);
           // let cookieValue = document.cookie.replace(
           //   /(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/,
           //   "$1"
